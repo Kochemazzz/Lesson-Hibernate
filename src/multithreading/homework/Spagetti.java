0eb18625c1387738e7b958b3e5876d867150ddd4
Iurii Kochemazov <kochemazov1@mail.ru>
2023-10-26 22:25:33 +0300

    i made code with on spoon.

--- src/multithreading/homework/Spagetti.java
+++ src/multithreading/homework/Spagetti.java
@@ -1,7 +1,7 @@
 package multithreading.homework;
 import java.util.Random;
 public class Spagetti {
-    private Random random = new Random();
+    private final  Random random = new Random();
     public int getSpagetti() {
         return random.nextInt(21); // кол-во спагетти
     }
